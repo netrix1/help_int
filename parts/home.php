27e291c933d2f03a3d980c2cadde1f83808f1bad
Everson Luiz Santana <urso.net@gmail.com>
2019-01-19 01:52:37 -0200

    Criação da pagina home

---
+++ parts/home.php
@@ -0,0 +1,49 @@
+<style type="text/css">
+	.breadcrumb {
+		margin: 0px;
+	}
+</style>
+
+<div> <!-- content-block -->
+	<div class="row no-gutters"> <!-- row -->
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://cdn.allwallpaper.in/wallpapers/1920x1200/536/chicago-skyline-at-night-1920x1200-wallpaper.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://picfiles.alphacoders.com/218/218407.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://cdn.allwallpaper.in/wallpapers/1920x1200/4998/boat-on-beautiful-sea-and-sky-1920x1200-wallpaper.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+	</div> <!-- end row -->
+</div>
+
+<?php $scripsjs="
+<script type='text/javascript'>
+$('.carousel').carousel({
+  interval: 10000
+})
+  
+</script>
+";?>
