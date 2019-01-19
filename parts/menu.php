39b6261c294c291bfbb8e1e0341b115fde208fd2
Everson Luiz Santana <urso.net@gmail.com>
2019-01-19 01:52:54 -0200

    Ediçõ da página de menu

--- parts/menu.php
+++ parts/menu.php
@@ -4,9 +4,57 @@
 							<a class="nav-link wave active" href="index-2.html"><span class="gg-icon material-icons">home</span>Home</a>
 						</li>
 
+
+						<li class="nav-item nav-toggle">
+							<a class="nav-link wave" href="javascript:void(0);"><span class="mdi mdi-toolbox-outline"></span>Links e ferramentas</a>
+							<ul class="nav-dropdown">
+								<li class="nav-item">
+									<a class="nav-link wave" href="pages/layouts/layout-1.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave d-flex" href="pages/layouts/layout-icon.html">
+										<span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout Icon 
+										<div class="ml-auto"><span class="badge badge-warning">new</span></div>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave" href="pages/layouts/no-sidebar-header.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
+								</li>
+							</ul>
+						</li>
+
 						<li class="nav-item">
-							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-help-circle-outline"></span><!--span class="gg-icon material-icons">question_answer</span-->FAQ</a>
-							
+							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-clipboard-check-outline"></span>Banco de informações</a>
+						</li>
+
+						<li class="nav-item nav-toggle">
+							<a class="nav-link wave" href="javascript:void(0);"><span class="mdi mdi-account-card-details"></span>Area do RH</a>
+							<ul class="nav-dropdown">
+								<li class="nav-item">
+									<a class="nav-link wave" href="pages/layouts/layout-1.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave d-flex" href="pages/layouts/layout-icon.html">
+										<span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout Icon 
+										<div class="ml-auto"><span class="badge badge-warning">new</span></div>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave" href="pages/layouts/no-sidebar-header.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
+								</li>
+							</ul>
+						</li>
+
+						<li class="nav-item">
+							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-help-circle-outline"></span>FAQ</a>
+						</li>
+
+						<li class="nav-item">
+							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-ticket-confirmation"></span>Chamados</a>
+						</li>
+
+						<li class="nav-item">
+							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-newspaper"></span>Notícias</a>
 						</li>
 
 					</ul>
