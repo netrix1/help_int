2ca449ac340463fcffd29a505a872f1159c4bc7e
Everson Luiz Santana <urso.net@gmail.com>
2019-01-20 16:25:13 -0200

    Conserto Link

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL_SITE', "http://localhost:8090/prj_intra"); 		// Caminho até a pasta de inicio home (index.php)
+define('URL_SITE', "http://localhost/help_int"); 		// Caminho até a pasta de inicio home (index.php)
 define('PASTA_PARTS', "parts"); 							// Pasta Onde está os recortes do site
 
 //====================== DEFINIÇÕES DA PAGINA
