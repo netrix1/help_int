dc56db2b2c3d1e369b3734807d93b1940406af67
netrix1 <urso.net@gmail.com>
2019-01-21 11:13:09 -0200

    Inclusão de categorias em tickts e remoção de links

--- parts/menu.php
+++ parts/menu.php
@@ -1,7 +1,7 @@
 					<ul class="nav flex-column sidebar-nav">
 
 						<li class="nav-item">
-							<a class="nav-link wave active" href="index-2.html"><span class="gg-icon material-icons">home</span>Home</a>
+							<a class="nav-link wave active" href="#"><span class="gg-icon material-icons">home</span>Home</a>
 						</li>
 
 
@@ -9,16 +9,16 @@
 							<a class="nav-link wave" href="javascript:void(0);"><span class="mdi mdi-toolbox-outline"></span>Links e ferramentas</a>
 							<ul class="nav-dropdown">
 								<li class="nav-item">
-									<a class="nav-link wave" href="pages/layouts/layout-1.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link wave d-flex" href="pages/layouts/layout-icon.html">
+									<a class="nav-link wave d-flex" href="#">
 										<span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout Icon 
 										<div class="ml-auto"><span class="badge badge-warning">new</span></div>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link wave" href="pages/layouts/no-sidebar-header.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
 								</li>
 							</ul>
 						</li>
@@ -31,16 +31,16 @@
 							<a class="nav-link wave" href="javascript:void(0);"><span class="mdi mdi-account-card-details"></span>Area do RH</a>
 							<ul class="nav-dropdown">
 								<li class="nav-item">
-									<a class="nav-link wave" href="pages/layouts/layout-1.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout-1</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link wave d-flex" href="pages/layouts/layout-icon.html">
+									<a class="nav-link wave d-flex" href="#">
 										<span class="gg-icon material-icons font-18 ml-1">adjust</span>Layout Icon 
 										<div class="ml-auto"><span class="badge badge-warning">new</span></div>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link wave" href="pages/layouts/no-sidebar-header.html"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>No Sidebar Header</a>
 								</li>
 							</ul>
 						</li>
@@ -49,12 +49,24 @@
 							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-help-circle-outline"></span>FAQ</a>
 						</li>
 
-						<li class="nav-item">
-							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-ticket-confirmation"></span>Chamados</a>
+
+						<li class="nav-item nav-toggle">
+							<a class="nav-link wave" href="javascript:void(0);"><span class="mdi mdi-ticket-confirmation"></span>Chamados</a>
+							<ul class="nav-dropdown">
+								<li class="nav-item">
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Operacional</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Suporte</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link wave" href="#"><span class="gg-icon material-icons font-18 ml-1">adjust</span>Departamento Pessoal</a>
+								</li>
+							</ul>
 						</li>
 
 						<li class="nav-item">
-							<a class="nav-link wave" href="helper-classes.html"><span class="mdi mdi-newspaper"></span>Notícias</a>
+							<a class="nav-link wave" href="#"><span class="mdi mdi-newspaper"></span>Notícias</a>
 						</li>
 
 					</ul>
