d9daa6405ea5fe47478151438463599b43bcc7c3
netrix1 <urso.net@gmail.com>
2019-01-23 14:18:18 -0200

    Criação de pagina de funções

---
+++ functions.php
@@ -0,0 +1,77 @@
+<?php
+function tipoNivel($tpNivel){
+	switch ($tpNivel) {
+    case 1:
+        echo "Operador";
+        break;
+    case 2:
+        echo "Operador - Especial";
+        break;
+    case 3:
+        echo "Operador - Especial";
+        break;
+    case 4:
+        echo "Operador - Especial";
+        break;
+    case 5:
+        echo "Operador - Especial";
+        break;
+    case 6:
+        echo "Recursos Humanos";
+        break;
+    case 7:
+        echo "Recursos Humanos - Especial";
+        break;
+    case 8:
+        echo "Recursos Humanos - Especial";
+        break;
+    case 9:
+        echo "BackOffice";
+        break;
+    case 10:
+        echo "Reservado BackOffice";
+        break;
+    case 11:
+        echo "Reservado BackOffice";
+        break;
+    case 12:
+        echo "Reservado BackOffice";
+        break;
+    case 21:
+        echo "Administrativo";
+        break;
+    case 22:
+        echo "Reservado Administrativo";
+        break;
+    case 23:
+        echo "Reservado Administrativo";
+        break;
+    case 24:
+        echo "Reservado Administrativo";
+        break;
+    case 25:
+        echo "Reservado Administrativo";
+        break;
+    case 26:
+        echo "Reservado Administrativo";
+        break;
+    case 27:
+        echo "Reservado Administrativo";
+        break;
+    case 28:
+        echo "Reservado Administrativo";
+        break;
+    case 29:
+        echo "Reservado Administrativo";
+        break;
+    case 30:
+        echo "Administrador";
+        break;
+    case 31:
+        echo "Reservado Administrador";
+        break;
+    case 32:
+        echo "Master / TI";
+        break;
+	}
+}
