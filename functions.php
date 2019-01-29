44144586e01b3b692351e99b560da419c7b97e85
netrix1 <urso.net@gmail.com>
2019-01-29 13:22:55 -0200

    Correção funções
    corrigido função que pega o nome do tipo do usuário e incluido função que cria select dos tipos de usuário.

--- functions.php
+++ functions.php
@@ -1,77 +1,29 @@
 <?php
-function tipoNivel($tpNivel){
-	switch ($tpNivel) {
-    case 1:
-        echo "Operador";
-        break;
-    case 2:
-        echo "Operador - Especial";
-        break;
-    case 3:
-        echo "Operador - Especial";
-        break;
-    case 4:
-        echo "Operador - Especial";
-        break;
-    case 5:
-        echo "Operador - Especial";
-        break;
-    case 6:
-        echo "Recursos Humanos";
-        break;
-    case 7:
-        echo "Recursos Humanos - Especial";
-        break;
-    case 8:
-        echo "Recursos Humanos - Especial";
-        break;
-    case 9:
-        echo "BackOffice";
-        break;
-    case 10:
-        echo "Reservado BackOffice";
-        break;
-    case 11:
-        echo "Reservado BackOffice";
-        break;
-    case 12:
-        echo "Reservado BackOffice";
-        break;
-    case 21:
-        echo "Administrativo";
-        break;
-    case 22:
-        echo "Reservado Administrativo";
-        break;
-    case 23:
-        echo "Reservado Administrativo";
-        break;
-    case 24:
-        echo "Reservado Administrativo";
-        break;
-    case 25:
-        echo "Reservado Administrativo";
-        break;
-    case 26:
-        echo "Reservado Administrativo";
-        break;
-    case 27:
-        echo "Reservado Administrativo";
-        break;
-    case 28:
-        echo "Reservado Administrativo";
-        break;
-    case 29:
-        echo "Reservado Administrativo";
-        break;
-    case 30:
-        echo "Administrador";
-        break;
-    case 31:
-        echo "Reservado Administrador";
-        break;
-    case 32:
-        echo "Master / TI";
-        break;
-	}
+
+function tpNivel($tpNivel){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+  require_once 'conexao.php';
+  $conexao = conexao::getInstance();
+  $sql = "SELECT nome FROM `tab_usernivel` WHERE `nivel` = ".$tpNivel;
+  $stm = $conexao->prepare($sql);
+  $stm->execute();
+  $retorno = $stm->fetch(PDO::FETCH_OBJ);
+  echo $retorno->nome;
+  unset($stm);
+}
+
+function selectUserLvl(){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+  require_once 'conexao.php';
+  $conexao = conexao::getInstance();
+  $sql = "SELECT id, nivel, nome FROM `tab_usernivel`";
+  $stm = $conexao->prepare($sql);
+  $stm->execute();
+
+  $result = $stm -> fetchAll();
+  $html ="<select class=\"form-control\">";
+  foreach( $result as $row ) {
+    $html.= "<option value=\"".$row['nivel']."\">".$row['nome']."</option>";
+  }
+  $html.="</select>";
+  unset($stm);
+  return $html;
 }
