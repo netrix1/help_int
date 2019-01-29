8ec7eaab7969fd873ab51e2642668394cf38a910
netrix1 <urso.net@gmail.com>
2019-01-29 16:20:11 -0200

    deixando pagina funcional

--- parts/makeuser.php
+++ parts/makeuser.php
@@ -11,47 +11,72 @@
 	<div class="row no-gutters">
 
 
-			<div class="col-12 col-md-6 col-lg-6 p-2">
-              <div class="card flat">
-                <div class="card-header bg-primary flat text-white py-3"><span class="mdi mdi-account-settings"></span> Criação de Usuário</div>
-                <div class="card-body">
-                  <div class="form-group form-group-icon form-material">
-                    <input class="form-control" type="text" placeholder="Nome do agente">
-                    <span class="form-border"></span>
-                    <span class="form-icon material-icons mdi mdi-account-outline"></span>
-                  </div>
-                  <div class="form-group form-group-icon form-material">
-                    <input class="form-control" type="text" placeholder="Usuario">
-                    <span class="form-border"></span>
-                    <span class="form-icon material-icons mdi mdi-account-key-outline"></span>
-                  </div>
-                  <div class="form-group form-group-icon form-material">
-                    <input class="form-control" type="password" placeholder="Senha">
-                    <span class="form-border"></span>
-                    <span class="form-icon material-icons mdi mdi-lock-outline"></span>
-                  </div>
-                  <div class="form-group form-group-icon form-material">
-                    <select class="form-control">
-                      <optgroup class="text-muted">Sexo</optgroup>
-                      <option>Masculino</option>
-                      <option>Feminino</option>
-                    </select>
-                    <span class="form-border"></span>
-                    <span class="form-icon material-icons mdi mdi-account-supervisor"></span>
-                  </div>
-                  <div class="form-group form-group-icon form-material">
-                    <textarea class="form-control" rows="1" placeholder="Textarea" style="margin-top: 0px; margin-bottom: 0px; height: 164px;"></textarea>
-                    <span class="form-border"></span>
-                    <span class="form-icon material-icons">chat_bubble_outline</span>
-                  </div>
-                  <div class="form-group form-group-icon form-material">
-                    <input class="form-control pl-0" type="text" placeholder="Search">
-                    <span class="form-border"></span>
-                    <span class="form-icon-right material-icons ml-0">search</span>
-                  </div>
-                </div>
-              </div>
+			<div class="col-12 col-md-12 col-lg-12 p-12">
+				<div class="card flat">
+					<div class="card-header bg-primary flat text-white py-3"><span class="mdi mdi-account-settings"></span> Criação de Usuário</div>
+
+<div class="row">
+  <div class="col-12 col-md-6 col-lg-6 p-2">
+    <form id="login-form" class="form-horizontal" role="form" action="insere.php" method="post" autocomplete="off">
+
+					<div class="card-body">
+						<div class="form-group form-group-icon form-material">
+							<input class="form-control" type="text" placeholder="Nome do agente" id="nome" name="nome">
+							<span class="form-border"></span>
+							<span class="form-icon material-icons mdi mdi-account-outline"></span>
+						</div>
+						<div class="form-group form-group-icon form-material">
+							<input class="form-control" type="text" placeholder="Usuario" id="user" name="user" autocomplete="off">
+							<span class="form-border"></span>
+							<span class="form-icon material-icons mdi mdi-account-key-outline"></span>
+						</div>
+						<div class="form-group form-group-icon form-material">
+							<input class="form-control" type="password" placeholder="Senha" id="senha" name="senha" autocomplete="off">
+							<span class="form-border"></span>
+							<span class="form-icon material-icons mdi mdi-lock-outline"></span>
+						</div>
+            <div class="form-group form-group-icon form-material">
+              <select class="form-control" placeholder="sexo" name="sexo">
+                <!--optgroup label="Sexo">Sexo</optgroup-->
+                <option disabled hidden selected>Sexo</option>
+                <option value="M">Masculino</option>
+                <option value="F">Feminino</option>
+              </select>
+              <span class="form-border"></span>
+              <span class="form-icon material-icons mdi mdi-account-supervisor"></span>
             </div>
+						<div class="form-group form-group-icon form-material">
+							<?php echo selectUserLvl("userLvl"); ?>
+							<span class="form-border"></span>
+							<span class="form-icon material-icons mdi mdi-account-star-outline"></span>
+						</div>
+
+						<div class="form-group form-group-icon form-material">
+              <input class="form-control" type="email" placeholder="E-mail" id="email" name="email" autocomplete="off">
+              <span class="form-border"></span>
+              <span class="form-icon material-icons mdi mdi-email-outline"></span>
+            </div>
+
+            <div class="form-group form-group-icon form-material">
+              <?php echo selectPai("pai"); ?>
+              <span class="form-border"></span>
+              <span class="form-icon material-icons mdi mdi-account-child-circle"></span>
+            </div>
+
+            <div class="form-group text-left">
+              <button type="submit" class="btn btn-primary shadow-2 flat">Incluir Usuário</button>
+              <button type="reset" class="btn btn-secondary shadow-2 flat">Limpar Cadastro</button>
+            </div>
+
+					</div>
+
+    </form>
+  </div>
+</div>
+				</div>
+			</div>
+
+
 
 		<!-- end row -->
 	</div>
@@ -59,8 +84,8 @@
 <?php $scripsjs="
 	<script type='text/javascript'>
 	$('.carousel').carousel({
-	  interval: 8000
+		interval: 8000
 	})
-	  
+		
 	</script>
 	";?>
