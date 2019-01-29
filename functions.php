53c3de33bd62b9818bdafb42fb7a6736a3df9351
netrix1 <urso.net@gmail.com>
2019-01-29 16:19:19 -0200

    funções editadas

--- functions.php
+++ functions.php
@@ -11,17 +11,36 @@ function tpNivel($tpNivel){ //retorn um select com todos os Niveis de usuarios c
   unset($stm);
 }
 
-function selectUserLvl(){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+function selectUserLvl($nomeID){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
   require_once 'conexao.php';
   $conexao = conexao::getInstance();
-  $sql = "SELECT id, nivel, nome FROM `tab_usernivel`";
+  $sql = "SELECT id, nivel, nome FROM `tab_usernivel` WHERE `status` = \"A\"";
   $stm = $conexao->prepare($sql);
   $stm->execute();
 
   $result = $stm -> fetchAll();
-  $html ="<select class=\"form-control\">";
+  $html ="<select class=\"form-control\" name=\"".$nomeID."\">";
+  $html.="<option disabled hidden selected>Nível de Previlégio do usuario</option>";
   foreach( $result as $row ) {
     $html.= "<option value=\"".$row['nivel']."\">".$row['nome']."</option>";
+  }
+  $html.="</select>";
+  unset($stm);
+  return $html;
+}
+
+function selectPai($nomeID){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+  require_once 'conexao.php';
+  $conexao = conexao::getInstance();
+  $sql = "SELECT id, nome FROM `tab_usuario`  WHERE `userLvl` = 6 or `userLvl`= 9 or `userLvl`= 21 or `userLvl`= 30 or `userLvl`= 31 or `userLvl`= 32 ORDER BY `userLvl` ASC";
+  $stm = $conexao->prepare($sql);
+  $stm->execute();
+
+  $result = $stm -> fetchAll();
+  $html ="<select class=\"form-control\" name=\"".$nomeID."\">";
+  $html.="<option disabled hidden selected>Escolha o Usuário Responsavel</option>";
+  foreach( $result as $row ) {
+    $html.= "<option value=\"".$row['id']."\">".$row['nome']."</option>";
   }
   $html.="</select>";
   unset($stm);
