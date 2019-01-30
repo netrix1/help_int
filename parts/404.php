51ba1a536962b9174e33db4f5f99532c12ea2e23
netrix1 <urso.net@gmail.com>
2019-01-30 17:26:46 -0200

    pagina de erro

---
+++ parts/404.php
@@ -0,0 +1,12 @@
+<div class="row no-gutters my-1">
+	<div class="col-sm-0 col-md-2 col-lg-2 p-1"></div>
+	<div class="col-sm-12 col-md-8 col-lg-8 p-1 margin-top-100">
+		<div class="info-box-2 w-100 hover-2">
+			<div class="info-box-icon"><span class="mdi mdi-alert-circle-outline text-warning gg-icon"></span></div> 
+			<div class="info-box-body">
+				<div class="info-box-subtitle">ERRO!</div>
+				<div class="info-box-title">A Página que você tentou entrar não existe ou foi removida!</div>
+			</div>
+		</div>
+	</div>
+</div>
