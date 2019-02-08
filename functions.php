7a750dff9a6f3181c39dcdbbf3b2326f0a035376
netrix1 <urso.net@gmail.com>
2019-02-08 11:44:28 -0200

    functions reformulada

--- functions.php
+++ functions.php
@@ -1,77 +1,120 @@
 <?php
-function tipoNivel($tpNivel){
-	switch ($tpNivel) {
-    case 1:
-        echo "Operador";
-        break;
-    case 2:
-        echo "Operador - Especial";
-        break;
-    case 3:
-        echo "Operador - Especial";
-        break;
-    case 4:
-        echo "Operador - Especial";
-        break;
-    case 5:
-        echo "Operador - Especial";
-        break;
-    case 6:
-        echo "Recursos Humanos";
-        break;
-    case 7:
-        echo "Recursos Humanos - Especial";
-        break;
-    case 8:
-        echo "Recursos Humanos - Especial";
-        break;
-    case 9:
-        echo "BackOffice";
-        break;
-    case 10:
-        echo "Reservado BackOffice";
-        break;
-    case 11:
-        echo "Reservado BackOffice";
-        break;
-    case 12:
-        echo "Reservado BackOffice";
-        break;
-    case 21:
-        echo "Administrativo";
-        break;
-    case 22:
-        echo "Reservado Administrativo";
-        break;
-    case 23:
-        echo "Reservado Administrativo";
-        break;
-    case 24:
-        echo "Reservado Administrativo";
-        break;
-    case 25:
-        echo "Reservado Administrativo";
-        break;
-    case 26:
-        echo "Reservado Administrativo";
-        break;
-    case 27:
-        echo "Reservado Administrativo";
-        break;
-    case 28:
-        echo "Reservado Administrativo";
-        break;
-    case 29:
-        echo "Reservado Administrativo";
-        break;
-    case 30:
-        echo "Administrador";
-        break;
-    case 31:
-        echo "Reservado Administrador";
-        break;
-    case 32:
-        echo "Master / TI";
-        break;
+
+function tpNivel($tpNivel){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+	require_once 'conexao.php';
+	$conexao = conexao::getInstance();
+	$sql = "SELECT nome FROM `tab_usernivel` WHERE `nivel` = ".$tpNivel;
+	$stm = $conexao->prepare($sql);
+	$stm->execute();
+	$retorno = $stm->fetch(PDO::FETCH_OBJ);
+	echo $retorno->nome;
+	unset($stm);
+}
+
+function selectUserLvl($nomeID){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+	require_once 'conexao.php';
+	$conexao = conexao::getInstance();
+	$sql = "SELECT id, nivel, nome FROM `tab_usernivel` WHERE `status` = \"A\"";
+	$stm = $conexao->prepare($sql);
+	$stm->execute();
+
+	$result = $stm -> fetchAll();
+	$html ="<select class=\"form-control\" name=\"".$nomeID."\">";
+	$html.="<option disabled hidden selected>Nível de Previlégio do usuario</option>";
+	foreach( $result as $row ) {
+		$html.= "<option value=\"".$row['nivel']."\">".$row['nome']."</option>";
 	}
+	$html.="</select>";
+	unset($stm);
+	return $html;
+}
+
+function selectPai($nomeID){ //retorn um select com todos os Niveis de usuarios cadastrados no banco de dados
+	require_once 'conexao.php';
+	$conexao = conexao::getInstance();
+	$sql = "SELECT id, nome FROM `tab_usuario`	WHERE `userLvl` = 6 or `userLvl`= 9 or `userLvl`= 21 or `userLvl`= 30 or `userLvl`= 31 or `userLvl`= 32 ORDER BY `userLvl` ASC";
+	$stm = $conexao->prepare($sql);
+	$stm->execute();
+
+	$result = $stm -> fetchAll();
+	$html ="<select class=\"form-control\" name=\"".$nomeID."\">";
+	$html.="<option disabled hidden selected>Escolha o Usuário Responsavel</option>";
+	foreach( $result as $row ) {
+		$html.= "<option value=\"".$row['id']."\">".$row['nome']."</option>";
+	}
+	$html.="</select>";
+	unset($stm);
+	return $html;
+}
+
+function pegaUsuario($id){// pega o nome do usuario pela ID
+	require_once 'conexao.php';
+	$conexao = conexao::getInstance();
+	$sql = "SELECT nome FROM `tab_usuario` WHERE `id` = ".$id;
+	$stm = $conexao->prepare($sql);
+	$stm->execute();
+	$result = $stm -> fetch();
+	$result = $result[0];
+	return $result;
+}
+
+function criaNome(){ // cria nome aleatórios para a utilização nas fotos de usuarios
+		$tamanho = mt_rand(11,13);
+		$all_str = "abcdefghijlkmnopqrstuvxyzwABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		$nome = "";
+		for ($i = 0;$i <= $tamanho;$i++){
+			$nome .= $all_str[mt_rand(0,61)];
+		}
+		return $nome;
+}
+
+function limita_caracteres($texto, $limite, $quebra = true){ // Limita caracteres de um texto com ou sem a quebra da palavra
+	$tamanho = strlen($texto);
+	if($tamanho <= $limite){ //Verifica se o tamanho do texto é menor ou igual ao limite
+		$novo_texto = $texto;
+	}else{ // Se o tamanho do texto for maior que o limite
+		if($quebra == true){ // Verifica a opção de quebrar o texto
+			$novo_texto = trim(substr($texto, 0, $limite))." [...]";
+		}else{ // Se não, corta $texto na última palavra antes do limite
+			$ultimo_espaco = strrpos(substr($texto, 0, $limite), " "); // Localiza o útlimo espaço antes de $limite
+			$novo_texto = trim(substr($texto, 0, $ultimo_espaco))." [...]"; // Corta o $texto até a posição localizada
+		}
+	}
+	return $novo_texto; // Retorna o valor formatado
+}
+
+
+function tempo_corrido($time) { // mostra tempo da postagem de forma amigável
+date_default_timezone_set('America/Sao_Paulo');
+$now = strtotime(date('m/d/Y H:i:s'));
+	$time = strtotime($time);
+	$diff = $now - $time;
+	$seconds = $diff;
+	$minutes = round($diff / 60);
+	$hours = round($diff / 3600);
+	$days = round($diff / 86400);
+	$weeks = round($diff / 604800);
+	$months = round($diff / 2419200);
+	$years = round($diff / 29030400);
+	if ($seconds <= 60) return "Agora pouco";
+	else if ($minutes <= 60) return $minutes==1 ?'1 min atrás':$minutes.' minu atrás';
+	else if ($hours <= 24) return $hours==1 ?'1 hrs atrás':$hours.' horas atrás';
+	else if ($days <= 7) return $days==1 ?'1 dia atras':$days.' dias atrás';
+	else if ($weeks <= 4) return $weeks==1 ?'1 semana atrás':$weeks.' semanas atrás';
+	else if ($months <= 12) return $months == 1 ?'1 mês atrás':$months.' meses atrás';
+	else return $years == 1 ? 'um ano atrás':$years.' anos atrás';
+}
+
+function pegaFoto($id){
+	require_once 'conexao.php';
+	$conexao = conexao::getInstance();
+	$sql = "SELECT foto FROM `tab_usuario` WHERE `id` = ".$id;
+	$stm = $conexao->prepare($sql);
+	$stm->execute();
+	$result = $stm -> fetch();
+	$result = $result[0];
+	if ($result == "") {
+		$result="defaut.png";
+	}
+	return $result;
 }
